429f42d9671f421cbe4fa1cb976fbb37794444a1
Strontiumsun <izzyhall25@gmail.com>
2019-03-03 20:31:50 -0600

    the final version is as far as I could get the game to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,10 @@
 window.onload = function () {
+    // THE OVERALL STATUS OF THE GAME
+    // this game kinda works, but it doesn't work 100%. Basically, you can win, but you cannot lose.
+    // I know there's something about the double loops in my checkWord function that is preventing the lose counter from working.
+    // However, I cannot get the game to even halfway work without them. Hence, I'm at an impasse.
+    // I asked for help from many different people, but I still couldn't fix the game. But I prettied up the page for you TA's.
+
 
     //my global variables
     // the array of the different words
@@ -14,60 +20,48 @@ window.onload = function () {
     var lives;
     var guessesLeft = 9;
     var showLives = document.getElementById("guesses-left");
+    var guessHere = document.getElementById("guess-here");
+
 
     // the wordPicker function displays the chosen word and one of the letters in the word in the console 
     function wordPicker() {
         console.log(currentWord);
         console.log(currentWord[3]);
     }
-    //calling the wordPicker function
     wordPicker();
 
     //this function generates a number of _ based on the length of the chosen word.
     function answerArray() {
         for (var i = 0; i < currentWord.length; i++) {
             placeHolder.push(" _ ");
-            // placeHolder[i] = " _ ";
         }
         console.log(placeHolder);
         var screen = document.getElementById("placeholder");
         // got this from a cheat sheet https://gist.github.com/thegitfather/9c9f1a927cd57df14a59c268f118ce86#add-elements-to-the-dom
         var h1Text = document.createTextNode(placeHolder.join(""));
         screen.appendChild(h1Text);
-
     }
     answerArray();
 
+    // this function updates the placeholders once the user has pressed a correct letter
     function pageUpdate() {
         var screen = document.getElementById("placeholder");
-        // var h1Text = document.createTextNode(placeHolder.join(""));
         screen.innerHTML = placeHolder.join("");
     }
-    // got this from another site
-    // comments = function () {
-    //     showLives.innerHTML = "You have " + lives + " lives";
-    //     if (lives < 1) {
-    //         showLives.innerHTML = "Game Over";
-    //     }
-    //     for (var i = 0; i < geusses.length; i++) {
-    //         if (counter + space === geusses.length) {
-    //             showLives.innerHTML = "You Win!";
-    //         }
-    //     }
-    // }
 
+    // this function is how the user's key presses are processed
     document.onkeyup = function (event) {
         console.log(event.key);
         userText.textContent = event.key;
         userGuess.push(event.key);
         console.log(userGuess);
-        livesDecrease();
-        checkWord();
-
+        checkWord(userGuess);
 
     }
 
+    // this is the main function and the one giving me problems. It loops too many times to be able to give a single integer for the win/lose condition, but I cannot get it to work without both loops
     function checkWord() {
+
         for (var i = 0; i < userGuess.length; i++) {
 
             for (var j = 0; j < currentWord.length; j++) {
@@ -78,63 +72,57 @@ window.onload = function () {
                     pageUpdate();
                     displayGuesses();
                     winGame();
+                    console.log(placeHolder.join(""));
                 }
                 else {
                     console.log("no match");
                     displayGuesses();
+                    livesDecrease();
                     winGame();
+                    // this code doesn't work. It doesn't console log properly, and it does not decrease the number of lives.
+                    // it does, however, correctly display the guesses you guess.
                 }
-            }
-        }
-    }
 
-    //every time we guess, the guess is pushed into the array
-    //at the end of check word we want to update the page
-    // make a function that if you correctly guess or wrongly guess
+            }
+
+        }
+
+    }
+    // this function displays the correctly guessed letters in place of the _'s
     function displayGuesses() {
-        var guessHere = document.getElementById("guess-here");
         guessHere.innerHTML = userGuess.join("");
     }
-
+    //  this function displays how many lives you have
     function lives() {
         showLives.innerHTML = "You have " + guessesLeft + " lives";
     }
     lives();
+    // this function should decrease the lives, but it does not work
     function livesDecrease() {
-        guessesLeft = guessesLeft--;
+        guessesLeft--;
     }
-
+    // this function lets you win, but you cannot lose.
     function winGame() {
-        if (guessesLeft < 1) {
-            showLives.innerHTML = "Game Over";
+        console.log("test " + (currentWord));
+        console.log("test " + (placeHolder.join("")));
+        if ((currentWord) === (placeHolder.join(""))) {
+            console.log("you win!");
+            showLives.innerHTML = ("You win!");
+            // PSUEDOCODE
+            // display button
+            // if click on button, reset game by picking a new word and returning the placeholder to its original state
+
         }
-        for (var i = 0; i < currentWord.length; i++) {
-            if (currentWord[i] === userGuess[i]) {
-                showLives.innerHTML = "You Win!"
-            }
+        else {
+            // if I put anything here, you lose right away
         }
+
     }
-
-
-    // comments = function () {
-    //     showLives.innerHTML = "You have " + lives + " lives";
-    //     if (lives < 1) {
-    //         showLives.innerHTML = "Game Over";
-    //     }
-    //     for (var i = 0; i < currentWord.length; i++) {
-    //         if (counter + space === currentWord.length) {
-    //             showLives.innerHTML = "You Win!";
-    //         }
-    //     }
-    // }
-
-
-
-
-
-
-
 
 
 
 }
+
+
+
+
